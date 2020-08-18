0db0b39b5061c02326c26f2f6a18283015af475d
moauris <mchenf@icloud.com>
2020-08-18 12:47:21 +0800

    Created first version of the app

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# SerialToSQLConditionConverter
-Desktop App to convert texts with numberic serial numbers to sql Where conditions
+# Serial To SQL Condition Converter
+WPF Desktop App to convert texts with numberic serial numbers to sql Where conditions.
+
+## Installation
+
+The project is a demo, compile the project in Microsoft Visual Studio.
+
+## User's Guide
+
+#### 1. Run the app
+
+![image-20200818124449031](C:\Users\MoChen\AppData\Roaming\Typora\typora-user-images\image-20200818124449031.png)
+
+#### 2. Paste the target text to the left textbox
+
+![image-20200818124536282](C:\Users\MoChen\AppData\Roaming\Typora\typora-user-images\image-20200818124536282.png)
+
+![image-20200818124612303](C:\Users\MoChen\AppData\Roaming\Typora\typora-user-images\image-20200818124612303.png)
+
+#### 3. Click on [Translate] button
+
+![image-20200818124647148](C:\Users\MoChen\AppData\Roaming\Typora\typora-user-images\image-20200818124647148.png)
+
---
+++ SerialToSQLConditionConverter/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace SQLWhereConditionGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnWindowMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void OnTranslateClicked(object sender, RoutedEventArgs e)
+        {
+            //Regex Logic to match your number type
+            // Current: match digit [0-9] with exactly 14 repititions
+            Regex r = new Regex(@"\d{14}");
+
+            string CheckedText = tbxPaste.Text;
+
+            MatchCollection mc = r.Matches(CheckedText);
+            Debug.Print($"There are {mc.Count} Matches found");
+
+            //Making a string builder to caputre the result
+            StringBuilder sb = new StringBuilder();
+            foreach (Match m in mc)
+            {
+                Debug.Print($"Caputured value: {m.Value}");
+                sb.Append('"');
+                sb.Append(m.Value);
+                sb.Append('"');
+                sb.Append(',');
+            }
+
+            //Truncate the last ',' character
+            int lastindex = sb.Length;
+            sb.Remove(lastindex - 1, 1);
+
+            tbxDisplay.Text = sb.ToString();
+
+
+        }
+
+        private void OnCloseClicked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
